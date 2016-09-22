dbeb00fc963abfe97ea9026d9089aff031741a5d
SteeliteInternational <kbrogley@steeliteusa.com>
2016-09-22 10:20:56 -0400

    Update test.php

--- test.php
+++ test.php
@@ -1,13 +1,14 @@
-1 <!DOCTYPE html PUBLIC “-//W3C//DTD XHTML 1.0 Transitional//EN” “http://www.w3.org/ TR/xhtml1/DTD/xhtml1-transitional.dtd”>
-2 <html xmlns=”http://www.w3.org/1999/xhtml” xml:lang=”en” lang=”en”>
-3 <head>
-4 <meta http-equiv=”content-type” content= ”text/html; charset=iso-8859-1” />
-5 <title>Using Echo()</title>
-6 </head>
-7 <body>
-8 <p>This is standard HTML.</p>
-9 <?php
-10 echo ‘This was generated using PHP!’;
-11 ?>
-12 </body>
-13 </html
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My First JavaScript</h1>
+
+<button type="button"
+onclick="document.getElementById('demo').innerHTML = Date()">
+Click me to display Date and Time.</button>
+
+<p id="demo"></p>
+
+</body>
+</html>
